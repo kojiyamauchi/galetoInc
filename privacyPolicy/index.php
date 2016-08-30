a7dc67b5c924c657b7f49a6dc676dbb106888b8d
kojiyamauchi <all.the.small.things@me.com>
2016-08-30 20:43:02 +0900

    making privacy policy page. all file update

---
+++ privacyPolicy/index.php
@@ -0,0 +1,123 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="content-language" content="ja">
+<title>プライバシーポリシー / 株式会社ジェラート</title>
+<meta name="title" content="" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=1440">
+<meta name="description" content="">
+<meta name="keywords" content="">
+<meta property="og:type" content="website" />
+<meta property="og:url" content="" />
+<meta property="og:title" content="" />
+<meta property="og:description" content="" />
+<meta property="og:image" content="" />
+<meta property="og:site_name" content="" />
+<link rel="apple-touch-icon" href="images/favicons/" />
+<link rel="shortcut icon" href="../images/favicons/favicon.ico" />
+<link rel="icon" type="image/png" href="../images/favicons/" sizes="">
+<link rel="icon" type="image/png" href="../images/favicons/" sizes="">
+<link rel="icon" type="image/png" href="../images/favicons/" sizes="">
+<link rel="mask-icon" href="../images/favicons/" color="">
+<meta name="msapplication-TileColor" content="">
+<meta name="msapplication-TileImage" content="../images/favicons/">
+<meta name="theme-color" content="">
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+<!-- Font Awesome CSS --->
+<link rel="stylesheet" type="text/css" href="../css/font-awesome.min.css">
+<!-- Original CSS -->
+<link rel="stylesheet" type="text/css" href="../css/gelato.css">
+<!-- jQuery -->
+<script type="text/javascript" src="../js/jquery-3.0.0.min.js"></script>
+<script type="text/javascript" src="../js/jquery-ui.min.js"></script>
+<!-- js cookie -->
+<script type="text/javascript" src="../js/js.cookie.js"></script>
+<!-- Bootstrap Js -->
+<script type="text/javascript" src="../js/bootstrap.min.js"></script>
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<!-- Original Js -->
+<script type="text/javascript" src="../js/gelato.js"></script>
+</head>
+<body class="privacyPolicyIntro">
+<div id="loadingWrap">
+  <div id="loading"> <img src="../images/loadingIceCreamLogo.svg" alt="loadingIceCreamLogo" id="loadingIceCreamLogo"/> <img src="../images/loadingIceCreamLogoExclamation.svg" alt="loadingIceCreamLogoExclamation" id="loadingIceCreamLogoExclamation" class="exclamationAnimation"/> </div>
+</div>
+<section>
+  <div id="privacyPolicyPage">
+    <nav id="globalNavi">
+      <ul id="naviWrap">
+        <li> <a href="../#topPage"><img src="../images/gelatoLogoFontBlack.svg" alt="gelatoLogoFontBlack"/></a> </li>
+        <li> <a href="../#aboutPage">ABOUT<span class="underLine">-</span></a> </li>
+        <li> <a href="../#worksPage">WORKS<span class="underLine">-</span></a> </li>
+        <li> <a href="../#companyPage">COMPANY<span class="underLine">-</span></a> </li>
+        <li id="linkGallery"> <a href="https://www.instagram.com/gelatoinc/" target="_blank">GALLERY<span class="underLine">-</span></a> <span id="instagramIcon">Link To Gelato Inc's Instagram!</span></li>
+        <li> <a href="../recruit">RECRUIT<span class="underLine">-</span></a> </li>
+        <li class="current"> <a href="../privacyPolicy">PRIVACY POLICY<span class="underLine">-</span></a> </li>
+        <li> <a href="../contact">CONTACT<span class="underLine">-</span></a> </li>
+      </ul>
+    </nav>
+    <div id="privacyPolicyContentsTop">
+      <div class="box">
+        <dl>
+          <dt>1.個人情報の管理</dt>
+          <dd>当社は、個人情報を正確かつ最新の状態に保ち、個人情報への不正アクセス・紛失・破損・改ざん・漏洩などを防止するため、セキュリティシステムの維持・管理体制の整備・社員教育の徹底等の必要な措置を講じ、安全対策を実施し個人情報の厳重な管理を行ないます。</dd>
+        </dl>
+      </div>
+      <div class="box">
+        <dl>
+          <dt>2.個人情報の利用目的</dt>
+          <dd>当社は、個人情報の取得、利用にあたっては、その利用目的を特定することとし、特定された利用目的の達成に必要な範囲を超えた個人情報の取扱い（目的外利用）はいたしません。<br>
+            また、目的外利用を行わないために、適切な管理措置を講じます。</dd>
+        </dl>
+      </div>
+      <div class="box">
+        <dl>
+          <dt>3.個人情報の第三者への開示・提供の禁止</dt>
+          <dd>当社は、個人情報の取得、利用にあたっては、その利用目的を特定することとし、特定された利用目的の達成に必要な範囲を超えた個人情報の取扱い（目的外当社は、本人の同意を得ている場合や法令に基づく場合等を除き、取得した個人情報を第三者に提供することはいたしません。</dd>
+        </dl>
+      </div>
+    </div>
+    <div id="mainImage">
+      <h1><span>P</span><span>R</span><span>I</span><span>V</span><span>A</span><span>C</span><span>Y</span> <span></span><span>P</span><span>O</span><span>L</span><span>I</span><span>C</span><span>Y</span></h1>
+      <p>株式会社ジェラート（以下、当社という）は、以下のとおり個人情報保護方針を定め、個人情報保護の仕組みを構築し、<br>
+        全従業員に個人情報保護の重要性の認識と取組みを徹底させることにより、個人情報の保護を推進致します。</p>
+      <p>
+        個人情報の取扱に関するお問い合せ先<br>
+      株式会社ジェラート  個人情報保護担当<br>
+      東京都世田谷区太子堂2-14-3  アーバンリゾート三軒茶屋4F<br>
+      <span>E-Mail：</span><a href="mailto:info@gelato-inc.com">info@gelato-inc.com<span class="underLine">-</span></a>
+      </p>
+    </div>
+    <div id="privacyPolicyContentsBottom">
+      <div class="box">
+        <dl>
+          <dt>4.個人情報の安全対策</dt>
+          <dd>当社は、個人情報の正確性及び安全性確保のために、セキュリティ等に万全の対策を講じ、個人情報の漏えい、滅失またはき損の防止及び是正に取り組みます。</dd>
+        </dl>
+      </div>
+      <div class="box">
+        <dl>
+          <dt>5.苦情および相談等に対する適正な対応</dt>
+          <dd>当社は、個人情報の取扱いに関する苦情及び相談を受けた場合は、その内容について迅速に事実関係等を調査し、適法かつ遅滞なく対応いたします。</dd>
+        </dl>
+      </div>
+      <div class="box">
+        <dl>
+          <dt>6.法令、規範の遵守と見直し</dt>
+          <dd>当社は、保有する個人情報に関して適用される日本の法令、その他規範を遵守するとともに、本ポリシーの内容を適宜見直し、その改善に努めます。</dd>
+        </dl>
+      </div>
+    </div>
+    <p id="copyRight"> COPYRIGHT ©<span><?php echo date('Y'); ?></span> GELATO Inc. </p>
+  </div>
+</section>
+</body>
+</html>
